15401af08dde3211ab7798177e3a43ee6515ca58
Yuri <voltermaher@gmail.com>
2021-01-20 00:21:00 +0700

    first commit

---
+++ src/components/AppComments.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "AppComments"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppSidebar.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "AppSidebar"
+}
+</script>
+
+<style scoped>
+
+</style>
